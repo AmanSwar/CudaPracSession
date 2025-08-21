adfb2b7734e1e4b974cee99a3f0d2db425bc1da9
Aman <aman.swar001@gmail.com>
2025-08-21 21:15:39 +0530

    add benchmark function

--- cute/matrix_addition/util.cuh
+++ cute/matrix_addition/util.cuh
@@ -68,3 +68,51 @@ bool checker(float *cublas_out, float *kernel_out) {
 
   return true;
 }
+
+void benchmark(void(launch_func)(float *, float *, float *)) {
+  size_t bytes = M*N * sizeof(float);
+
+  std::vector<float> ha(M*N), hb(M*N), h_kernelout(M*N), h_cublasout(M*N);
+  for (int i = 0; i < M*N; ++i) {
+    ha[i] = static_cast<float>(i);
+    hb[i] = static_cast<float>(i * 2);
+  }
+
+  float *da, *db, *d_kernelout, *d_cublasout;
+  cudaMalloc(&da, bytes);
+  cudaMalloc(&db, bytes);
+  cudaMalloc(&d_kernelout, bytes);
+  cudaMalloc(&d_cublasout, bytes);
+
+  cudaMemcpy(da, ha.data(), bytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(db, hb.data(), bytes, cudaMemcpyHostToDevice);
+
+  launch_func(da, db, d_kernelout);
+  cudaEvent_t start, end;
+  cudaEventCreate(&start);
+  cudaEventCreate(&end);
+
+  cudaEventRecord(start);
+  for (int i = 0; i < 100; i++) {
+    launch_func(da, db, d_kernelout);
+  }
+  cudaEventRecord(end);
+  cudaEventSynchronize(end);
+
+  float ms = 0.0f;
+  cudaEventElapsedTime(&ms, start, end);
+  std::cout << "Kernel TIME : " << ms / 100 << std::endl;
+
+  cublas_matAdd(da, db, d_cublasout);
+  cudaMemcpy(d_kernelout, h_kernelout.data(), sizeof(float) * N,
+             cudaMemcpyDeviceToHost);
+  cudaMemcpy(d_cublasout, h_cublasout.data(), sizeof(float) * N,
+             cudaMemcpyDeviceToHost);
+
+  std::cout << checker(h_kernelout.data(), h_cublasout.data()) << std::endl;
+
+  delete da;
+  delete db;
+  delete d_kernelout;
+  delete d_cublasout;
+}
